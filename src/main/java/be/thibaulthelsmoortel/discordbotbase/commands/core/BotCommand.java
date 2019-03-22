6fe76dda0cf0b8a8756fa0353c98c01074828c5f
Thibstars <thibault.helsmoortel@gmail.com>
2019-03-22 19:18:23 +0100

    Enable help options by default, refactor command execution

--- src/main/java/be/thibaulthelsmoortel/discordbotbase/commands/core/BotCommand.java
+++ src/main/java/be/thibaulthelsmoortel/discordbotbase/commands/core/BotCommand.java
@@ -2,6 +2,7 @@ package be.thibaulthelsmoortel.discordbotbase.commands.core;
 
 import java.util.concurrent.Callable;
 import net.dv8tion.jda.core.events.Event;
+import picocli.CommandLine.Option;
 
 /**
  * Command definition providing execution context.
@@ -9,6 +10,10 @@ import net.dv8tion.jda.core.events.Event;
  * @author Thibault Helsmoortel
  */
 public abstract class BotCommand implements Callable {
+
+    @SuppressWarnings("unused")
+    @Option(names = {"-h", "--help"}, usageHelp = true, description = "Prints this help message and exits.")
+    private boolean helpRequested;
 
     private Event event;
 
